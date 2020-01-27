95cc5989d6cb2d4dfc2ea21c7685fa8b38744c66
beecallpaw <thapabeecallpaw@gmail.com>
2020-01-27 22:50:37 +0545

    events emit

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var io = require('socket.io')(http)
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
-io.on('connection', socket => console.log('user connected'))
+io.on('connection', function(socket) {
+    console.log('user connected')
+    socket.on('message', function(msg){
+        console.log('message:'+ msg)
+    })
+    socket.on('disconnect', function(){
+        console.log('user disconnected')
+    })
+})
 
 http.listen(3000, () => console.log('listening on port 3000'))
